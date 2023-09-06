96fed313152a55ca88e10002af64ea93ffc7be89
Marcoss-Freire <marcos.freire5@etec.sp.gov.br>
2023-09-06 20:34:58 -0300

    Cria a estruta básica da tela de cuidado

---
+++ app/src/main/java/com/example/app_sustentavel/Cuidados.java
@@ -0,0 +1,14 @@
+package com.example.app_sustentavel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cuidados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuidados);
+    }
+}
