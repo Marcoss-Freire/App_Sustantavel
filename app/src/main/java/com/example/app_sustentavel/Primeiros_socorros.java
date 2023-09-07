380cf0ffa52d239851b29dd2730606244265c1e1
Marcoss-Freire <marcos.freire5@etec.sp.gov.br>
2023-09-07 14:04:12 -0300

    Adicionando a tela de primeiros socorros

---
+++ app/src/main/java/com/example/app_sustentavel/Primeiros_socorros.java
@@ -0,0 +1,14 @@
+package com.example.app_sustentavel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Primeiros_socorros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeiros_socorros);
+    }
+}
---
+++ app/src/main/java/com/example/app_sustentavel/Tutoriais.java
@@ -0,0 +1,14 @@
+package com.example.app_sustentavel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tutoriais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutoriais);
+    }
+}
