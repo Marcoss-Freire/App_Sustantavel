01f6b932b0c7ccbddf6bedc51e177129debf115c
Marcoss-Freire <marcos.freire5@etec.sp.gov.br>
2023-09-13 22:24:54 -0300

    Programando tela de perfil

--- app/src/main/java/com/example/app_sustentavel/Cadastro.java
+++ app/src/main/java/com/example/app_sustentavel/Cadastro.java
@@ -2,13 +2,72 @@ package com.example.app_sustentavel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Cadastro extends AppCompatActivity {
+
+    EditText txt_nome, txt_sobrenome, txt_email, txt_id, txt_telefone;
+    Button btn_cadastrar;
+    ImageButton btn_home, btn_video, btn_perfil;
+    String nome, sobrenome, id, email, telefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        txt_nome = findViewById(R.id.edt_txt_nome);
+        txt_sobrenome = findViewById(R.id.edit_txt_sobrenome);
+        txt_id = findViewById(R.id.edt_txt_id);
+        txt_email = findViewById(R.id.edt_txt_email);
+        txt_telefone = findViewById(R.id.edt_txt_telefone);
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+        btn_home = findViewById(R.id.imgbtn_home);
+        btn_video = findViewById(R.id.imgbtn_video);
+        btn_perfil = findViewById(R.id.imgbtn_perfil);
+
+        btn_cadastrar.setOnClickListener(v -> {
+            nome = String.valueOf(txt_nome.getText());
+            sobrenome = String.valueOf(txt_sobrenome.getText());
+            id = String.valueOf(txt_id.getText());
+            email = String.valueOf(txt_email.getText());
+            telefone = String.valueOf(txt_telefone.getText());
+
+            if (nome.equals("Nome") || sobrenome.equals(getString(R.string.sobrenome_exe)) || id.equals("@pet123") || email.equals("example1@email.com") || telefone.equals("(DD) XXXXX-XXXX")) {
+                Toast erro = Toast.makeText(this, "Preencha todos os campos e tente novamente", Toast.LENGTH_LONG);
+                erro.show();
+            }
+            else {
+                Intent cadastro = new Intent(this, Perfil.class);
+                cadastro.putExtra("nome", nome);
+                cadastro.putExtra("sobrenome", sobrenome);
+                cadastro.putExtra("id", id);
+                cadastro.putExtra("email", email);
+                cadastro.putExtra("telefone", telefone);
+                startActivity(cadastro);
+            }
+        });
+
+        btn_home.setOnClickListener(v -> {
+            Intent home = new Intent(this, MainActivity.class);
+            startActivity(home);
+        });
+
+        btn_video.setOnClickListener(v -> {
+            Intent video = new Intent(this, Tutoriais.class);
+            startActivity(video);
+        });
+
+        btn_perfil.setOnClickListener(v -> {
+            Intent perfil = new Intent(this, Perfil.class);
+            startActivity(perfil);
+        });
     }
 }
--- app/src/main/java/com/example/app_sustentavel/Perfil.java
+++ app/src/main/java/com/example/app_sustentavel/Perfil.java
@@ -2,13 +2,75 @@ package com.example.app_sustentavel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.chip.Chip;
 
 public class Perfil extends AppCompatActivity {
+
+    LinearLayout LLbase;
+    ScrollView SCRbase;
+    Button btn_cadastro;
+    TextView txt_nome_usu, txt_id_usu, txt_nome, txt_sobrenome, txt_id, txt_email, txt_telefone;
+    Chip visibilidade;
+    String nome, sobrenome, id, email, telefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        LLbase = findViewById(R.id.LLbase_false);
+        SCRbase = findViewById(R.id.SCRbase);
+        btn_cadastro = findViewById(R.id.btn_cadastro);
+        txt_nome_usu = findViewById(R.id.txt_nome_usu);
+        txt_id_usu = findViewById(R.id.txt_id);
+        txt_nome = findViewById(R.id.txt_nome);
+        txt_id = findViewById(R.id.txt_id_dados);
+        txt_sobrenome = findViewById(R.id.txt_sobrenome);
+        txt_email = findViewById(R.id.txt_email);
+        txt_telefone = findViewById(R.id.txt_telefone);
+        visibilidade = findViewById(R.id.chip_visibilidade);
+
+        Bundle dados = getIntent().getExtras();
+
+        if(dados != null){
+            nome = dados.getString("nome");
+            sobrenome = dados.getString("sobrenome");
+            id = dados.getString("id");
+            email = dados.getString("email");
+            telefone = dados.getString("telefone");
+
+            txt_nome_usu.setText(nome);
+            txt_id_usu.setText(id);
+
+            txt_nome.setText(txt_nome.getText() + nome);
+            txt_sobrenome.setText(txt_sobrenome.getText() + sobrenome);
+            txt_id.setText(txt_id.getText() + id);
+            txt_email.setText(txt_email.getText() + email);
+            txt_telefone.setText(txt_telefone.getText() + telefone);
+        }
+
+        if(nome != null || sobrenome != null || id != null || email != null ||telefone != null){
+            SCRbase.setVisibility(View.VISIBLE);
+
+        }
+        else{
+            LLbase.setVisibility(View.VISIBLE);
+
+            btn_cadastro.setOnClickListener(v -> {
+                Intent ir_cad = new Intent(this, Cadastro.class);
+                startActivity(ir_cad);
+            });
+
+        }
     }
 }
